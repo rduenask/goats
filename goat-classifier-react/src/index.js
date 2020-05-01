f8ffa20f89a778c7e9ce42d403b859a2e296dbf8
Ricardo Duenas <ricardo.duenas@gmail.com>
2020-05-01 14:18:52 -0500

    react app

--- goat-classifier-react/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import FlowerClassifier from './components/FlowerClassifier';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<FlowerClassifier />, document.getElementById('root'));
-
-serviceWorker.unregister();
